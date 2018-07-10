e81de94e4f45c3e946247dcc5f75d3d3bdec8710
hallessandro.dvilla <hallessandro.dvilla@esig.com.br>
2018-07-10 14:00:35 -0300

    Add constantes para facilitar alterações futuras

--- gerarInSql.py
+++ gerarInSql.py
@@ -16,4 +16,7 @@ def montarQuery(nomeTabela, nomeCampo, valores):
     listaValores = ', '.join(valores)
     return '''SELECT * FROM {nomeTabela} WHERE {nomeCampo} IN ({valores})'''.format(nomeTabela=nomeTabela, nomeCampo=nomeCampo, valores=listaValores);
     
-print(montarQuery('teste', 'teste2', montaString('arquivos/lista.txt')))
+ARQUIVO = 'arquivos/lista.txt' #Caminho do arquivo que vai ser lido   
+NOME_TABELA = 'schema.tabela' #Nome da tabela que será feito o Select
+CAMPO_COMPARADOR = 'nome_campo' #Campo será feita o IN
+print(montarQuery(NOME_TABELA, CAMPO_COMPARADOR, montaString(ARQUIVO)))
